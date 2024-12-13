75736d71a3cd4cf0bd58a4a129df8ff897819eec
Jimmy Vaniski <vaniskito@hotmail.com>
2024-12-13 18:04:57 +0000

    chore: add git  commit message linter

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ const name = "";
 console.log(name);
 const home = () => {
   return <div className="fill mx-6 w-full bg-red-500 px-2 py-5">olá mundo</div>;
+  <a>estou testando isso</a>;
 };
 
 export default home;
