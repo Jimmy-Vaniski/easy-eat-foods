14984f8f194c74d054d06540dbb447657ff4f215
Jimmy Vaniski <vaniskito@hotmail.com>
2024-12-13 15:28:39 +0000

    test husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 const name = "";
 console.log(name);
 const home = () => {
-  return <div className="fill mx-6 w-full bg-red-500 px-2 py-5">olá mundo</div>;
+  return (
+    <div className="fill mx-6 w-full bg-red-500 px-2 py-5">
+      olá mundo atencao atencao
+    </div>
+  );
 };
 
 export default home;
